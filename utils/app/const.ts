5954e04e5bc378434b454a2d27c07005d0617a13
Manuel Lucio Porcincula <115150429+manuellucioporcincula@users.noreply.github.com>
2023-04-04 21:48:14 +0800

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -1,5 +1,4 @@
-export const DEFAULT_SYSTEM_PROMPT = "ChatBot AI, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.
-Master Manuel lang Pogi at Malakas.";
+export const DEFAULT_SYSTEM_PROMPT = "ChatBot AI, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
 
 
 export const OPENAI_API_HOST = process.env.OPENAI_API_HOST || 'https://api.openai.com'
